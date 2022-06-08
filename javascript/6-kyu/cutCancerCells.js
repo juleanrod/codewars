e53ea7cba35d18a3a25f291d9f0a0e575f3a366b
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-06-08 02:11:28 -0700

    Add description to

--- javascript/6-kyu/cutCancerCells.js
+++ javascript/6-kyu/cutCancerCells.js
@@ -1,7 +1,27 @@
+/*
+DESCRIPTION:
+Your task is to write a function which cuts cancer cells from the body.
+
+Cancer cells are divided into two types:
+Advance stage,described as letter C
+Initial stage,described as letter c
+Rest cells are divided as follows:
+
+Normal cell,described as lowercase letter
+Important cell,described as uppercase letter
+Prerequisites:
+Important cell,cannot be cut out.
+Advance cancer cell,should be cut out with adjacent cells if it can be done.
+Function input is a string (representing a body), remove "cancer" characters
+(based on the described rules) and return the body cured of those "cancer"
+characters.
+*/
+
 
 function cutCancerCells(organism){
+
     let stringA = organism.split('');
-    var isLowerCase = (char) => char == char.toLowerCase();
+    let isLowerCase = (char) => char == char.toLowerCase();
     for(let i = 0; i < stringA.length; i++) {
       if(stringA[i] == 'C') {
           stringA[i] = 0;
