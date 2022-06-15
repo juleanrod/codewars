229040a67e8958f7538c75a6b550c679ddf28403
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-06-15 00:36:51 -0700

    Add implementation to findInArray.js

---
+++ javascript/6-kyu/findInArray.js
@@ -0,0 +1,30 @@
+/*
+DESCRIPTION:
+We'll create a function that takes in two parameters:
+
+a sequence (length and types of items are irrelevant)
+a function (value, index) that will be called on members of the sequence and
+their index. The function will return either true or false.
+Your function will iterate through the members of the sequence in order until
+the provided function returns true; at which point your function will return
+that item's index.
+
+If the function given returns false for all members of the sequence, your
+function should return -1.
+
+var trueIfEven = function(value, index) { return (value % 2 === 0) };
+findInArray([1,3,5,6,7], trueIfEven) // should === 3
+*/
+
+
+const findInArray = function(arr, iterator) {
+  for(let i = 0; i < arr.length; i++) {
+    if(iterator(arr[i], i)) {
+      return i;
+    };
+  }
+  
+  return -1;
+};
+
+// Time to complete: 12min;
