0073fe3964b4a9dd2bd197cc4666d8a87839e5f8
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-08-20 00:24:59 -0700

    add solution ticTacToe

--- javascript/5-kyu/ticTacToe.js
+++ javascript/5-kyu/ticTacToe.js
@@ -19,4 +19,46 @@
  * You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
 */
 
+function isSolved(board) {
+  const isWinner = (symbol, inputs) => inputs.every(cell => cell === symbol);
+  // check backward diagonal
+  const backDiagonal = [board[0][0], board[1][1], board[2][2]];
+  if (isWinner(1, backDiagonal)) {
+    return 1;
+  } else if (isWinner(2, backDiagonal)) {
+    return 2;
+  }
+  // check forward diagonal
+  const forwardDiagonal = [board[2][0], board[1][1], board[0][2]];
+  if (isWinner(1, forwardDiagonal)) {
+    return 1;
+  } else if (isWinner(2, forwardDiagonal)) {
+    return 2;
+  }
+  // check rows
+  for (let i = 0; i < 3; i++) {
+    const row = board[i];
+    if (isWinner(1, row)) {
+      return 1;
+    } else if (isWinner(2, row)) {
+      return 2;
+    }
+  }
+  // check columns
+  for (let i = 0; i < 3; i++) {
+    const col = [board[0][i], board[1][i], board[2][i]];
+    if (isWinner(1, col)) {
+      return 1;
+    } else if (isWinner(2, col)) {
+      return 2;
+    }
+  }
+  // check for 0's
+  if (board.some(row => row.some(cell => cell === 0))) {
+    return -1;
+  }
+  // draw
+  return 0;
+}
 
+// Time to complete: 39min
